8111a9ca1dd65e66f57f2f4c5842baca8f9f3152
Jussi Nurminen <jnu@iki.fi>
2016-10-18 13:53:20 +0300

    use acqparserfif

--- python/chpi_weighted_average.py
+++ python/chpi_weighted_average.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Weighted averaging of epochs according to continuous HPI (cHPI) signal-to-noise.
+Weighted averaging of epochs according to SNR of continuous HPI (cHPI) signal.
 
 
-@author: jussi
+@author: Jussi Nurminen (jnu@iki.fi)
 """
 
 
 from __future__ import print_function
 import matplotlib.pyplot as plt
 import mne
-from mne.io.constants import FIFF
 import numpy as np
 import argparse
-import sys
 import os
-import warnings
-from elekta_avg import Elekta_averager
+from mne import AcqParserFIF
+from mne.chpi import _get_hpi_info
 
 
 def chpi_freqs(info):
@@ -44,23 +42,27 @@ def chpi_snr_epochs(epochs, n_lineharm=2, channels='grad', hpi_coil='median'):
     if len(epochs.event_id) > 1:
         raise ValueError('Epochs object should contain one category only')
 
-    alldata = epochs.pick_types(meg=True).get_data()    
+    epochs.load_data()
+    alldata = epochs.pick_types(meg=True).get_data()
     nepochs, nchan, buflen = alldata.shape
     sfreq = epochs.info['sfreq']
     t = np.linspace(0, buflen/sfreq, endpoint=False, num=buflen)
     cfreqs = list(chpi_freqs(epochs.info))
+    # this cannot be used yet, see
+    # https://github.com/mne-tools/mne-python/pull/3670
+    #(cfreqs, _, _, _, _) = _get_hpi_info(epochs.info)
     ncoils = len(cfreqs)
     linefreq = epochs.info['line_freq']
     linefreqs = (np.arange(n_lineharm+1)+1) * linefreq
-    
+
     # gradiometer and magmeter indices
     pick_meg = mne.pick_types(epochs.info, meg=True)
     pick_mag = mne.pick_types(epochs.info, meg='mag')
     pick_grad = mne.pick_types(epochs.info, meg='grad')
 
-    # create linear model    
+    # create linear model
     model = np.c_[t, np.ones(t.shape)]  # model slope and DC
-    for f in list(linefreqs)+cfreqs:  # add sine and cosine term for each freq
+    for f in list(linefreqs)+list(cfreqs):  # add sine and cosine term for each freq
         model = np.c_[model, np.cos(2*np.pi*f*t), np.sin(2*np.pi*f*t)]
     inv_model = np.linalg.pinv(model)
 
@@ -70,58 +72,87 @@ def chpi_snr_epochs(epochs, n_lineharm=2, channels='grad', hpi_coil='median'):
     resid_vars = np.zeros([nchan, nepochs])
 
     for epn in range(nepochs):
-        epdata = alldata[epn,:,:].transpose()
+        epdata = alldata[epn, :, :].transpose()
         coeffs = np.dot(inv_model, epdata)
         coeffs_hpi = coeffs[2 + 2*len(linefreqs):]
-        resid_vars[:,epn] = np.var(epdata - np.dot(model, coeffs), 0)
+        resid_vars[:, epn] = np.var(epdata - np.dot(model, coeffs), 0)
         # get total hpi amplitudes by combining sine and cosine terms
-        hpi_amps = np.sqrt(coeffs_hpi[0::2,:]**2 + coeffs_hpi[1::2,:]**2)
+        hpi_amps = np.sqrt(coeffs_hpi[0::2, :]**2 + coeffs_hpi[1::2, :]**2)
         # divide average HPI power by average variance
-        snr_avg_grad[:,epn] = np.divide((hpi_amps**2/2)[:,pick_grad].mean(1),resid_vars[pick_grad,epn].mean())
-        snr_avg_mag[:,epn] = np.divide((hpi_amps**2/2)[:,pick_mag].mean(1),resid_vars[pick_mag,epn].mean())
+        snr_avg_grad[:, epn] = np.divide((hpi_amps**2/2)[:, pick_grad].mean(1),
+                                         resid_vars[pick_grad, epn].mean())
+        snr_avg_mag[:, epn] = np.divide((hpi_amps**2/2)[:, pick_mag].mean(1),
+                                        resid_vars[pick_mag, epn].mean())
 
     if hpi_coil == 'median':
         fun = np.median
     elif hpi_coil == 'best':
         fun = np.max
     elif hpi_coil == 'worst':
-        fun = np.min        
+        fun = np.min
     else:
-        raise ValueError('Invalid HPI coil selection parameter')        
+        raise ValueError('Invalid HPI coil selection parameter')
 
     if channels == 'grad':
         snr = fun(snr_avg_grad, axis=0)
     elif channels == 'mag':
         snr = fun(snr_avg_mag, axis=0)
-    
+
     return snr
 
+
 def weigh_epochs(epochs, weights):
-    """ Weigh epochs in mne.Epochs object (elementwise multiply by weights vector). """
+    """ Weigh epochs in mne.Epochs object (elementwise multiply by
+    weights vector). """
+    epochs.load_data()
     weights = np.array(weights)  # will accept either list or numpy array
     n_epochs = len(epochs)
     if not len(weights) == n_epochs:
         raise ValueError('Need as many weights as epochs')
-    w_ = weights.squeeze()[:,np.newaxis,np.newaxis]  # reshape for broadcasting
-    # normalize by n_epochs/sum(weights), so that averaging will yield a correct result
-    epochs._data *=  n_epochs * w_ / np.sum(w_)
+    # reshape for broadcasting
+    w_ = weights.squeeze()[:, np.newaxis, np.newaxis]
+    # normalize by n_epochs/sum(weights), so that averaging
+    # will yield a correct result
+    epochs._data *= n_epochs * w_ / np.sum(w_)
+
 
 if __name__ == '__main__':
-    
+
     """ Default parameters """
     default_nharm = 2  # number of line harmonics to include
-    default_stim_channel = 'STI101'    
-    
+    default_stim_channel = 'STI101'
+
     """ Parse command line """
-    parser = argparse.ArgumentParser(description='Weighted averaging of epochs according to cHPI SNR.')
-    parser.add_argument('snr_file', help='Name of raw fiff file. cHPI SNR will be computed from this file. Epochs for averaging will also be taken from this file, unless --epochs_file is specified.')
-    parser.add_argument('--epochs_file', type=str, default=None, help='Raw fiff file to use for averaging epochs. It must have the same epochs/categories as snr_file.')
-    parser.add_argument('--nharm', type=int, default=default_nharm, choices=[0,1,2,3,4], help='Number of line frequency harmonics to include')
-    parser.add_argument('--epoch_start', type=float, default=None, help='Epoch start relative to trigger (s)')
-    parser.add_argument('--epoch_end', type=float, default=None, help='Epoch end relative to trigger (s)')
-    parser.add_argument('--stim_channel', type=str, default=default_stim_channel, help='Stim (trigger) channel')
-    parser.add_argument('--plot_snr', type=bool, default=False, help='Whether to plot epochs-based SNR or not')    
-    parser.add_argument('--sti_mask', type=int, default=0, help='Mask to apply to the stim channel')    
+    parser = argparse.ArgumentParser(description='Weighted averaging of '
+                                                 'epochs according to cHPI '
+                                                 'SNR.')
+    help_snr_file = ('Name of raw fiff file. cHPI SNR will be computed from '
+                     'this file. Epochs for averaging will be taken from '
+                     'the same file, unless --epochs_file is specified.')
+    help_epochs_file = ('Raw fiff file to get the epochs from. It must '
+                        'have the same epochs/categories as snr_file.')
+    help_reject = ('Whether to use the rejection parameters defined in '
+                   'the data acquisition when averaging epochs.')
+    help_nharm = 'Number of line frequency harmonics to include.'
+    help_epoch_start = 'Epoch start relative to trigger (s)'
+    help_epoch_end = 'Epoch end relative to trigger (s)'
+    help_plot_snr = 'Whether to plot SNR or not'
+    help_sti_mask = 'Mask to apply to the stim channel'
+    parser.add_argument('snr_file', help=help_snr_file)
+    parser.add_argument('--epochs_file', type=str, default=None,
+                        help=help_epochs_file)
+    parser.add_argument('--reject', type=bool, default=False,
+                        help=help_reject)
+    parser.add_argument('--nharm', type=int, default=default_nharm,
+                        choices=[0, 1, 2, 3, 4], help=help_nharm)
+    parser.add_argument('--epoch_start', type=float, default=None,
+                        help=help_epoch_start)
+    parser.add_argument('--epoch_end', type=float, default=None,
+                        help=help_epoch_end)
+    parser.add_argument('--plot_snr', type=bool, default=False,
+                        help=help_plot_snr)
+    parser.add_argument('--sti_mask', type=int, default=0,
+                        help=help_sti_mask)
 
     args = parser.parse_args()
 
@@ -130,72 +161,61 @@ if __name__ == '__main__':
     else:
         fnbase = os.path.basename(os.path.splitext(args.snr_file)[0])
     verbose = False
-    
-    """ Load cHPI SNR file. It is typically not maxfiltered, so ignore MaxShield warnings. """
-    with warnings.catch_warnings():
-        warnings.simplefilter('ignore')
-        raw_chpi = mne.io.Raw(args.snr_file, allow_maxshield=True, verbose=verbose)
 
-    triggers = mne.find_events(raw_chpi, stim_channel=default_stim_channel, consecutive=True, verbose=verbose, mask=args.sti_mask)
+    """ Load cHPI SNR file. It is typically not maxfiltered, so ignore
+    MaxShield warnings. """
+    raw_chpi = mne.io.Raw(args.snr_file, allow_maxshield='yes',
+                          verbose=verbose)
     picks = mne.pick_types(raw_chpi.info, meg=True)
-    
+
     """ If using a separate file for the actual data epochs, load it. """
     if args.epochs_file:
-        raw_epochs = mne.io.Raw(args.epochs_file, allow_maxshield=True, verbose=verbose)
-    
-    """ Get averaging parameters. These should be identical for the SNR and epochs files. """
-    eav = Elekta_averager(raw_chpi.info['acq_pars'])
-    events, event_ids = eav.events_from_mne_triggers(triggers)
-    if args.epochs_file:
-        triggers_epochs = mne.find_events(raw_epochs, stim_channel=default_stim_channel, consecutive=True, verbose=verbose, mask=args.sti_mask)
-        if not np.all(triggers_epochs == triggers):
-            raise Exception('Epochs file has triggers different from cHPI file.')
+        raw_epochs = mne.io.Raw(args.epochs_file, allow_maxshield=True,
+                                verbose=verbose)
 
-    """ For each category, compute the SNR weights and the weighted average. """
-    evokeds = [] 
-    for catname, id in event_ids.iteritems():
-        print('\nProcessing category', id, ':', catname)
+    """ Get averaging parameters. These should be identical for the SNR and
+    epochs files. """
+    ap = AcqParserFIF(raw_chpi.info)
+
+    """ For each category, compute the SNR weights and the weighted
+    average. """
+    evokeds = []
+    for cat in ap.categories:
+        print('\nProcessing category: %s' % cat['comment'])
         print('Loading epochs for cHPI SNR...')
+        cond = ap.get_condition(raw_chpi, cat)
         if args.epoch_start:
             epoch_start = args.epoch_start
             epoch_end = args.epoch_end
         else:
-            epoch_start = eav.categories[catname].start
-            epoch_end = eav.categories[catname].end
-        chpi_epochs = mne.Epochs(raw_chpi, events, {catname: id}, tmin=epoch_start, tmax=epoch_end, baseline=None, picks=picks, preload=True, verbose=verbose)
-        print('Computing SNR...')        
-        w_snr = chpi_snr_epochs(chpi_epochs, n_lineharm=args.nharm, channels='grad', hpi_coil='median')
-        if args.plot_snr:
-            plt.figure()
-            plt.plot(20*np.log10(w_snr))
-            plt.xlabel('Epoch n')
-            plt.ylabel('SNR (dB)')
-            plt.title(catname)
+            epoch_start = cond['tmin']
+            epoch_end = cond['tmax']
+        reject = ap.reject if args.reject else None
+        flat = ap.flat if args.reject else None
+        chpi_epochs = mne.Epochs(raw_chpi, reject=reject, flat=flat, **cond)
+        print('Computing SNR...')
+        w_snr = chpi_snr_epochs(chpi_epochs, n_lineharm=args.nharm,
+                                channels='grad', hpi_coil='median')
         if not args.epochs_file:
             data_epochs = chpi_epochs
         else:
-            print('Loading epochs for averaging...')        
-            data_epochs = mne.Epochs(raw_epochs, events, {catname: id}, tmin=epoch_start, tmax=epoch_end, baseline=None, picks=picks, preload=True, verbose=verbose)
+            print('Loading epochs for averaging...')
+            data_epochs = mne.Epochs(raw_epochs, reject=reject, flat=flat,
+                                     **cond)
         print('Computing weighted average...')
         weigh_epochs(data_epochs, w_snr)
         evoked = data_epochs.average()
-        evoked.comment = catname   # average() should do this but doesn't
+        evoked.comment = cat['comment']
         evokeds.append(evoked)
 
     if args.plot_snr:
+        plt.figure()
+        plt.plot(20*np.log10(w_snr))
+        plt.xlabel('Epoch n')
+        plt.ylabel('SNR (dB)')
         plt.show()
 
     """ Write all resulting evoked objects to a fiff file. """
     fn = fnbase + '_chpi_weighted-ave.fif'
     print('Saving', fn)
     mne.write_evokeds(fn, evokeds)
-        
-        
-
-
-
-
-        
-        
-        
-        
