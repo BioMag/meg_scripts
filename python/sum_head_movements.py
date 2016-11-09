c0b7c2694a817733655c3288242c592d2d02db72
Jussi Nurminen <jnu@iki.fi>
2016-11-09 16:16:55 +0200

    wip

--- python/sum_head_movements.py
+++ python/sum_head_movements.py
@@ -14,28 +14,30 @@ import numpy as np
 import argparse
 from mne.transforms import quat_to_rot, rotation_angles, _angle_between_quats
 from scipy.signal import medfilt, filtfilt, butter
+from scipy.linalg import inv
 
 
 # lowpass filter
-def filt(y, sfrate, passband, axis=-1):
-    """ Filter given data y to passband, e.g. [1, 40].
-    Passband is given in Hz. Implemented as pure lowpass, 
-    if highpass freq = 0 """
+def lowpass(y, sfrate, corner, axis=-1):
+    """ lowpass filter given data """
     ord = 5
-    passbandn = 2 * np.array(passband) / sfrate
-    if passbandn[0] > 0:  # bandpass
-        b, a = butter(ord, passbandn, 'bandpass')
-    else:  # lowpass
-        b, a = butter(ord, passbandn[1])
-    yfilt = filtfilt(b, a, y, axis=axis)        
+    if corner is None:
+        return y
+    cornern = 2 * corner / sfrate
+    b, a = butter(ord, cornern)
+    yfilt = filtfilt(b, a, y, axis=axis)
     return yfilt
-
 
 # parse command line
 parser = argparse.ArgumentParser(description='Get head movement info from raw '
                                              'fiff file.')
 parser.add_argument('fiff_file', help='Name of raw fiff file')
+
 parser.add_argument('--plot', help='Plot HPI data', action='store_true')
+parser.add_argument('--lpcorner', type=int, default=10,
+                    help='Lowpass corner frequency for filtering position '
+                         'and angle data')
+
 args = parser.parse_args()
 
 print(args.plot)
@@ -45,7 +47,9 @@ DS_FREQ = 100  # frequency to downsample to
 
 raw = mne.io.Raw(args.fiff_file)
 if 'CHPI001' not in raw.info['ch_names']:
-    raise ValueError('Cannot find movement data in file')
+    raise ValueError('Cannot find movement data in file. File needs to be '
+                     'processed with MaxFilter first (movement estimation '
+                     'or compensation).')
 raw.load_data()
 raw.pick_types(meg=False, chpi=True)
 raw.resample(DS_FREQ)
@@ -77,57 +81,54 @@ Rq, _ = raw[picks_chpi_r, :]
 Rq = Rq[:, ind_ok].T
 
 # convert to rotations around x, y and z axes
-#R = quat_to_rot(Rq)
-#nrot = R.shape[0]
-#A = np.zeros((nrot, 3))
-#for k in np.arange(nrot):
-#    A[k, :] = np.array(rotation_angles(R[k, :, :]))/np.pi * 180
+R = quat_to_rot(Rq)
+nrot = R.shape[0]
+A = np.zeros((nrot, 3))
+for k in np.arange(nrot):
+    A[k, :] = np.array(rotation_angles(R[k, :, :]))/np.pi * 180
 
-# get angles directly
-Rq = filt(Rq, raw.info['sfreq'], [0, 5], axis=0)
+# filter quaternion data and get difference angles directly
+Rq = lowpass(Rq, raw.info['sfreq'], args.lpcorner, axis=0)
 dA = _angle_between_quats(Rq[1:, :], Rq[:-1, :]) / np.pi * 180
 
 # translation
 T, _ = raw[picks_chpi_t, :]
 T = T[:, ind_ok].T
-T = filt(T, raw.info['sfreq'], [0, 5], axis=0)
+T = lowpass(T, raw.info['sfreq'], args.lpcorner, axis=0)
 dT = np.diff(T, axis=0)
 dTv = np.sqrt(np.sum(dT**2, axis=1))  # magnitude of movement at each time point
 tlen = times_ok[-1] - times_ok[0]
 
 dTtot = np.sum(dTv)
 dAtot = np.sum(dA)
-print('Total head translation during recording: %.2f mm, average %.2f mm/s' % 
+print('\nTotal head translation during recording: %.2f mm, average %.2f mm/s' % 
       (dTtot*1e3, dTtot*1e3 / tlen))
-print('Total head rotation during recording: %.2f deg, average %.2f deg/s' % 
+print('Total head rotation during recording: %.2f deg, average %.2f deg/s\n' % 
       (dAtot, dAtot / tlen))
 
 if args.plot:
     plt.figure()
-    plt.subplot(3, 1, 1)
-    plt.plot(times_ok[1:], 1e3*dTv)
-    plt.title('Head translations (for GOF > %g)' % GOF_LIMIT)
-    plt.xlabel('Time (s)')
-    plt.ylabel('Translation (mm)')
-    
-    #plt.figure()
-    #plt.plot(times_all, g)
-    #plt.title('Goodness of fit (whole recording)')
-    #plt.xlabel('Time (s)')
-    #plt.ylabel('Translation (mm)')
-    
-    plt.subplot(3, 1, 2)
-    plt.plot(times_ok[1:], dA)
-    plt.title('Rotation')
-    plt.xlabel('Time (s)')
-    plt.ylabel('Rotation (deg)')
+    ax = plt.subplot(4, 1, 1)
+    ax.plot(times_ok[1:], 1e3*dTv)
+    ax.set_title('Head translations (where GOF > %g)' % GOF_LIMIT)
+    ax.set(ylabel='Translation (mm)')
 
-    plt.subplot(3, 1, 3)    
-    plt.plot(times_ok, T*1e3)
-    plt.title('Position')
-    plt.legend(['x','y','z'])
-    plt.xlabel('Time (s)')
-    plt.ylabel('Position (mm)')
+    ax = plt.subplot(4, 1, 2)
+    ax.plot(times_ok[1:], dA)
+    ax.set_title('Head rotations (where GOF > %g)' % GOF_LIMIT)
+    ax.set(ylabel='Rotation (deg)')
+
+    ax = plt.subplot(4, 1, 3)
+    ax.plot(times_ok, T*1e3)
+    ax.set_title('Position of head coordinate origin (whole file)')
+    ax.legend(['x','y','z'])
+    ax.set(ylabel='Position (mm)')
+
+    ax = plt.subplot(4, 1, 4)
+    ax.plot(times_all, g)
+    ax.set_title('Goodness of fit (whole recording)')
+    ax.set(ylabel='Translation (mm)', xlabel='Time (s)')
+    ax.set_ylim([.8, 1.05])
     
     plt.tight_layout()
    
