4241063d8fd041f0b9630593b4bf787e957154d2
Jussi Nurminen <jnu@iki.fi>
2018-12-17 17:39:05 +0200

    fix time vec

--- python/mdip_plot_waveforms.py
+++ python/mdip_plot_waveforms.py
@@ -31,19 +31,19 @@ def _datalines(fn):
             if line[0] != '#':
                 yield line
 
+
 def header(v):
     """Return header info from array v"""
     ndip, nsamp = v[0]
     t0, t1, dt = v[1]
-    t = np.arange(t0, t1, dt)
-    return ndip, t
+    return ndip, nsamp, t0, t1, dt
 
 
 def waveforms(v):
     """Return all dipole waveforms from array v"""
     ndip, nsamp = v[0]
     for k in range(int(ndip)):
-       yield v[4 + k * 3]
+        yield v[4 + k * 3]
         #dip = lines[3 + k * 3]
 
 
@@ -61,17 +61,21 @@ if __name__ == '__main__':
                         action='store_true')
     parser.add_argument('--legend', help='create a legend',
                         action='store_true')
+    parser.add_argument('--average', help='plot the average of all waveforms',
+                        action='store_true')
     args = parser.parse_args()
     dipn = args.dipn - 1  # 1-based index, 0 is last (becomes -1)
     waves_all = list()
-    
+
     for fn in args.fns:
         print(fn)
-        v = [np.fromstring(l, sep=' ') for l in _datalines(fn)]    
-        ndip, t = header(v)
+        v = [np.fromstring(l, sep=' ') for l in _datalines(fn)]
+        ndip, nsamp, t0, t1, dt = header(v)
+        t = np.linspace(t0, t1, int(nsamp))
         waves = list(waveforms(v))
         if args.dipn > ndip:
-            raise ValueError('Dipole #%d does not exist in %s' % (args.dipn, fn))
+            raise ValueError('Dipole #%d does not exist in %s' %
+                             (args.dipn, fn))
         wave = waves[dipn]
         plt.plot(t, wave)
         waves_all.append(wave)
@@ -83,15 +87,17 @@ if __name__ == '__main__':
     plt.xlabel('Time (ms)')
     plt.ylabel('Dipole amplitude (nAm)')
     ylim = plt.gca().get_ylim()
-    wall = np.array(waves_all)
-    wall_avg = wall.mean(axis=0)
-    plt.figure()
-    plt.plot(t, wall_avg)
-    plt.title('Averaged waveform (dipole %d)' % args.dipn)
-    plt.xlabel('Time (ms)')
-    plt.ylabel('Dipole amplitude (nAm)')
-    if not args.autoscale:
-        plt.gca().set_ylim(ylim[0], ylim[1])
+
+    if args.average:
+        plt.figure()
+        wall = np.array(waves_all)
+        wall_avg = wall.mean(axis=0)
+        plt.plot(t, wall_avg)
+        plt.title('Averaged waveform (dipole %d)' % args.dipn)
+        plt.xlabel('Time (ms)')
+        plt.ylabel('Dipole amplitude (nAm)')
+        if not args.autoscale:
+            plt.gca().set_ylim(ylim[0], ylim[1])
 
 
 
